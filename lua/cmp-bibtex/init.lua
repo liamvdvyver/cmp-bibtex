8b64fd7ffd1c6a40796d8f243acd17b3cf717fc7
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2023-09-19 15:35:53 +1000

    refactor: move helper to util.lua

--- lua/cmp-bibtex/init.lua
+++ lua/cmp-bibtex/init.lua
@@ -1,3 +1,4 @@
+local util = require("cmp-bibtex.util")
 local source = {}
 
 -- Set defaults
@@ -37,48 +38,13 @@ function source:get_trigger_characters()
   return { "@", "{" }
 end
 
-function source.get_field(entry, field)
-  return (string.match(entry, field .. "%s*=%s*%b{}") or "NA"):gsub(field .. "%s*=%s*", ""):gsub("[{}]", "")
-end
-
-function source.get_bibresources(context)
-  local ret = {}
-  if context.filetype == "tex" then
-    local lines = vim.api.nvim_buf_get_lines(context.bufnr, 0, -1, false)
-
-    for _, line in ipairs(lines) do
-      -- TODO: Support multiple per line
-      -- TODO: Support relative paths
-      -- TODO: Support other methods of adding bib resources
-      -- TODO: Exclude commented out lines
-      local matches = line:gmatch("\\addbibresource%b{}")
-
-      for match in matches do
-        local bib_file = match:gsub("(\\addbibresource{)(.*)(}%s*)", "%2")
-        table.insert(ret, bib_file)
-      end
-    end
-  end
-  return ret
-end
-
-function source.file_exists(file)
-  local f = io.open(file, "r")
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
-end
-
 function source:complete(params, callback)
   local isIncomplete = false
   local context = params.context
 
   local parsed_entries = {}
 
-  local files = vim.tbl_flatten({ source.opts.files, source.get_bibresources(context) })
+  local files = vim.tbl_flatten({ source.opts.files, util.get_bibresources(context) })
 
   local file_keys = {}
 
@@ -87,7 +53,7 @@ function source:complete(params, callback)
   end
 
   for file, _ in pairs(file_keys) do
-    if not source.file_exists(file) then goto continue end
+    if not util.file_exists(file) then goto continue end
 
     -- TODO: Expand ~/$HOME
     io.input(file)
@@ -98,9 +64,9 @@ function source:complete(params, callback)
 
       for entry in entries do
         local key             = entry:match("@%w*%s*{%c?%w*,?"):gsub("@%w*%s*{%c?", ""):gsub(",?", "")
-        local author          = source.get_field(entry, "author")
-        local year            = source.get_field(entry, "year")
-        local title           = source.get_field(entry, "title")
+        local author          = util.get_field(entry, "author")
+        local year            = util.get_field(entry, "year")
+        local title           = util.get_field(entry, "title")
 
         local completion_item = {
           label = key,
---
+++ lua/cmp-bibtex/util.lua
@@ -0,0 +1,38 @@
+M = {}
+
+function M.get_field(entry, field)
+  return (string.match(entry, field .. "%s*=%s*%b{}") or "NA"):gsub(field .. "%s*=%s*", ""):gsub("[{}]", "")
+end
+
+function M.get_bibresources(context)
+  local ret = {}
+  if context.filetype == "tex" then
+    local lines = vim.api.nvim_buf_get_lines(context.bufnr, 0, -1, false)
+
+    for _, line in ipairs(lines) do
+      -- TODO: Support multiple per line
+      -- TODO: Support relative paths
+      -- TODO: Support other methods of adding bib resources
+      -- TODO: Exclude commented out lines
+      local matches = line:gmatch("\\addbibresource%b{}")
+
+      for match in matches do
+        local bib_file = match:gsub("(\\addbibresource{)(.*)(}%s*)", "%2")
+        table.insert(ret, bib_file)
+      end
+    end
+  end
+  return ret
+end
+
+function M.file_exists(file)
+  local f = io.open(file, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+
+return M
